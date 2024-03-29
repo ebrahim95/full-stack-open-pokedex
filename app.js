cfc9c5784eb22f5e4af3c25626813a5aec75773a
Ebrahim Haji <ebrahim.akh95@gmail.com>
2024-03-29 17:36:37 -0500

    feat: banana route

--- app.js
+++ app.js
@@ -15,6 +15,11 @@ app.get('/coco', (req, res) => {
   res.send('it works coco')
 })
 
+app.get('/banana', (req, res) => {
+  res.send('it works banana')
+})
+
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on Port 5000')
